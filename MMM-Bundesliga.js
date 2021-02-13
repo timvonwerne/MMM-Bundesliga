aa4721695ed064493601ba152af5b441c2902dc2
Tim von Werne <me@vonwernet.de>
2021-02-13 16:14:00 +0100

    Setup API Calls

--- MMM-Bundesliga.js
+++ MMM-Bundesliga.js
@@ -1,30 +1,79 @@
 Module.register("MMM-Bundesliga", {
-	defaults: {
-		header: ""
-	},
+  defaults: {
+    header: "",
+    initialLoadDelay: 0,
+    updateInterval: 10 * 60 * 10000
+  },
 
-	getScripts() {
-		return ["moment.js"];
-	},
+  apiURL: "https://www.openligadb.de/api/getmatchdata/bl1",
 
-	getStyles() {
-		return ["MMM-Bundesliga.css"];
-	},
+  getScripts() {
+    return ["moment.js"];
+  },
 
-	start() {
-		Log.info("Starting module: " + this.name);
+  getStyles() {
+    return ["MMM-Bundesliga.css"];
+  },
 
-		moment.locale(config.language);
-	},
+  start() {
+    Log.info("Starting module: " + this.name);
 
-	getDom() {
-		const wrapper = document.createElement("div");
-		wrapper.className = this.config.tableClass;
+    moment.locale(config.language);
 
-		return wrapper;
-	},
+    this.scheduleUpdate(this.config.initialLoadDelay);
+  },
 
-	getHeader() {
-		return this.config.header;
-	}
+  getDom() {
+    const wrapper = document.createElement("div");
+    wrapper.className = this.config.tableClass;
+
+    return wrapper;
+  },
+
+  getHeader() {
+    return this.config.header;
+  },
+
+  updateScores: function () {
+    var self = this;
+    var retry = true;
+
+    var weatherRequest = new XMLHttpRequest();
+    //weatherRequest.setRequestHeader("accept", "application/json");
+    weatherRequest.open("GET", this.apiURL);
+
+    weatherRequest.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          Log.info("response", JSON.parse(this.response));
+          //self.processWeather(JSON.parse(this.response));
+        } else if (this.status === 401) {
+          self.updateDom(self.config.animationSpeed);
+
+          Log.error(self.name + ": Incorrect API-Key.");
+          retry = true;
+        } else {
+          Log.error(self.name + ": Could not load scores.");
+        }
+
+        if (retry) {
+          self.scheduleUpdate(self.loaded ? -1 : self.config.retryDelay);
+        }
+      }
+    };
+
+    weatherRequest.send();
+  },
+
+  scheduleUpdate: function (delay) {
+    var nextLoad = this.config.updateInterval;
+    if (typeof delay !== "undefined" && delay >= 0) {
+      nextLoad = delay;
+    }
+
+    var self = this;
+    setTimeout(function () {
+      self.updateScores();
+    }, nextLoad);
+  }
 });
